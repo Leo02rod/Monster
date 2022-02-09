ee96d9f346e96082b6250cffa10bf136f85c6f97
Leonardo Rodriguez <leorod0079@gmail.com>
2022-02-09 13:31:59 +1100

    work for 9/02/22

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SeachrBox
           placeholder='search monsters'
           handleChange={this.handleChange}
